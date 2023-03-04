c24ee4deddf66a71723c33744368562500dba0c3
Alexandr Romanov <37437841+Dragon925@users.noreply.github.com>
2023-03-04 20:02:38 +0300

    Initial commit

---
+++ app/src/main/java/com/github/dragon925/clockapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.dragon925.clockapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
